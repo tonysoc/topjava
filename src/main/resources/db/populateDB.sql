b949ce2b479afc9fe67e846f97d663a40f414ca6
tonysoc <tonysoc@mail.ru>
2017-04-20 15:15:48 +0300

    init and populate meals db

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -11,3 +12,7 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals(user_id, datetime, discription, calories) VALUES
+  (100000, '2017-01-12 12:06:00', 'Cookie', 300),
+  (100001, '2017-04-20 16:00:01', 'Beer', 600);
